6ca78cc126380a2fa75c5cd6baba05bd78c67ee6
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2017-01-19 19:29:58 +0000

    Move again scripts to footer and change jQuery version to latest

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -126,17 +126,13 @@
     </div>
 @endsection
 
-@section( 'headScripts' )
+@section( 'scripts' )
     @parent
-    {{-- Html::script( "https://unpkg.com/jquery@1.11.3/dist/jquery.min.js", [
-      "type"  => "text/javascript",
-      "async" => "",
-      "defer" => "",
-    ] ) --}}
-    {!! Html::script( "js/jquery-1.11.3.min.js", [
-      "type"  => "text/javascript",
-      "async" => "",
-      "defer" => "",
+    {!! Html::script( "https://unpkg.com/jquery@3.1.1/dist/jquery.min.js", [
+      "type"  => "text/javascript"
     ] ) !!}
+    {{-- Html::script( "js/jquery-1.11.3.min.js", [
+      "type"  => "text/javascript"
+    ] ) --}}
     {!! Html::script( asset( "js/index.js" ), [ "type" => "text/javascript" ] ) !!}
 @endsection
