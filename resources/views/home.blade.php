6eed30ddf2e39b9df6c2a43621659a44c50b01f5
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2017-01-20 21:51:07 +0000

    Adding a placeholder for prepare the site for a description.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,9 @@
 @extends( 'layouts.app' )
 
+@section( 'description', '' )
+
 @section( 'headScripts' )
-    @parent
+@parent
 @endsection
 
 @section( 'content' )
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,13 +3,13 @@
   <head>
     <meta charset="utf-8">
     <title>{{ config( 'app.name', 'ABC cachorro' ) }}</title>
-    {!! Html::meta( "description", "descripcion del sitio web" ) !!}
+    <meta name="description" content="@yield( 'description' )">
     {!! Html::meta( "viewport", "width=device-width, initial-scale=1" ) !!}
 
-    {{-- CSRF Token --}}
+    <!-- CSRF Token -->
     {!! Html::meta( "csrf-token", csrf_token() ) !!}
 
-    {{-- Favicon --}}
+    <!-- Favicon -->
     {!! Html::favicon( asset( 'favicon.png' ), [] ) !!}
     {!! Html::style( asset( 'cropped-icono_abc-32x32.jpg' ), [
       "rel"   => "icon",
@@ -26,24 +26,21 @@
       "name"  => "msapplication-TileImage"
     ] ) !!}
 
-    {{-- Scripts --}}
-    <script>
-      window.Laravel = <?php echo json_encode( [
-        'csrfToken' => csrf_token(),
-      ] ); ?>
-    </script>
+    <!-- Scripts -->
+    <script>window.Laravel = <?php echo json_encode( [ 'csrfToken' => csrf_token(), ] ); ?></script>
 
-    @section( 'headScripts' )
-      <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+
+@section( 'headScripts' )
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
         })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
         ga('create', 'UA-90625247-1', 'auto');
         ga('send', 'pageview');
-      </script>
-    @show
+    </script>
+@show
 
     <!-- Styles -->
     {!! Html::style( asset( 'css/app.css' ), [
