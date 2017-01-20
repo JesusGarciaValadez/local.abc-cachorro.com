afaa9b1ca336f4471bebe09d1c377bbc09e4c96b
jairo villalobos <jairo.villalobos@lunave.com>
2017-01-19 19:38:52 -0600

    agregando aceptar

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -6,7 +6,12 @@
         {!! Html::image( asset( 'images/logo-cachorro.png' ), "ABC Cachorro", [] ) !!}
       </figure>
 
-      <img src="https://ugc.kn3.net/i/origin/http://images.wikia.com/bleach/es/images/a/ab/Exito_meme.jpg">
+     <section class="validar-cerrar">
+        <figure class="logo"><img src="images/logo-cachorro.png"></figure>
+        <figure class="paloma"><img src="images/cerrar.png"></figure>
+        <h2>¡Gracias!</h2>
+        <p>Hemos recibido tus datos correctamente, muy pronto recibirás sorpresas exclusivas. </p>
+      </section>
 
       @foreach( $errors->all( ) as $error )
       <div class="alert alert-warning">
