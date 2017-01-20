cac7e9157d17a3c428e02384eb671cff2bb942ac
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2017-01-20 21:32:54 +0000

    Adding a space to separate Google analytics from another script

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,6 +32,7 @@
         'csrfToken' => csrf_token(),
       ] ); ?>
     </script>
+
     @section( 'headScripts' )
       <script>
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
