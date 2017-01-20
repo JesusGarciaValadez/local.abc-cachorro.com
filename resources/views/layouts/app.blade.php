ad924dc250180b7200a60a0740c651e3fcf93985
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2017-01-20 21:04:29 +0000

    Adding a favicon.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,12 +5,28 @@
     <title>{{ config( 'app.name', 'ABC cachorro' ) }}</title>
     {!! Html::meta( "description", "descripcion del sitio web" ) !!}
     {!! Html::meta( "viewport", "width=device-width, initial-scale=1" ) !!}
-    {!! Html::favicon( asset( 'favicon.png' ), [] ) !!}
 
-    <!-- CSRF Token -->
+    {{-- CSRF Token --}}
     {!! Html::meta( "csrf-token", csrf_token() ) !!}
 
-    <!-- Scripts -->
+    {{-- Favicon --}}
+    {!! Html::favicon( asset( 'favicon.png' ), [] ) !!}
+    {!! Html::style( asset( 'cropped-icono_abc-32x32.jpg' ), [
+      "rel"   => "icon",
+      "sizes" => "32x32"
+    ] ) !!}
+    {!! Html::style( asset( 'cropped-icono_abc-192x192.jpg' ), [
+      "rel"   => "icon",
+      "sizes" => "192x192"
+    ] ) !!}
+    {!! Html::style( asset( 'cropped-icono_abc-180x180.jpg' ), [
+      "rel"   => "apple-touch-icon-precomposed"
+    ] ) !!}
+    {!! Html::style( asset( 'cropped-icono_abc-270x270.jpg' ), [
+      "name"  => "msapplication-TileImage"
+    ] ) !!}
+
+    {{-- Scripts --}}
     <script>
       window.Laravel = <?php echo json_encode( [
         'csrfToken' => csrf_token(),
